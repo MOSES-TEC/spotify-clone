fdb0d2040a6cd1e3e40eb370f0ec44a782a08213
MOSES Y <ymoses546@gmail.com>
2025-08-14 23:09:20 +0530

    implementation of spotify

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllUsers, getMessages } from "../controller/user.controller.js";
+
+
+const router = Router();
+
+router.get("/", protectRoute, getAllUsers);
+
+// getMessages
+router.get("/messages/:userId", protectRoute, getMessages);
+
+
+export default router;
+
